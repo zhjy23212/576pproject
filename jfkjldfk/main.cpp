48c88cabcb65e28248e2c868ca63b7889ead4777
jyangz <raidenotaku@gmail.com>
2016-01-28 13:16:49 -0700

    Initial Commit

---
+++ jfkjldfk/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  jfkjldfk
+//
+//  Created by Jiyang on 16/1/28.
+//  Copyright © 2016年 Jiyang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
