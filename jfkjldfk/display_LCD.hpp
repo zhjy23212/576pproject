4095ba58540946a0e193ff081bde9cccf6e29356
jyangz <raidenotaku@gmail.com>
2016-04-12 14:00:49 -0700

    display functional added

--- jfkjldfk/display_LCD.hpp
+++ jfkjldfk/display_LCD.hpp
@@ -12,5 +12,23 @@
 #include <stdio.h>
 #include "bus_interface.h"
 #include "Bus_request.h"
+#include "simple_interface.h"
+
+class display:public sc_module, public simple_if{
+public:
+    std::vector<std::vector<unsigned int>> display_img;
+    unsigned int  done = 0;
+    sc_port<bus_slave_if> slv;
+    unsigned int larger = 0;
+    
+    
+    
+    
+    void listenThread(); //keep waiting and execute larger operation everytime
+    
+    bool Write ( unsigned  int addr,  unsigned  int data )  ;
+    bool Read ( unsigned  int addr,  unsigned int & data )  ;
+    
+};
 
 #endif /* display_LCD_hpp */
